48ddd91c4c9a3073f1d3757d0a94d00ee3e2d040
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-02-20 17:06:55 +0600

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="text-xl">
+            this is footer
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <div className="grid grid-cols-10">
+
+                <div>
+                    <h2>
+                        Earth Donor
+                    </h2>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
