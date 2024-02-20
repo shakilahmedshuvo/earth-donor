fcee0728d24e17c4a85c9cfda0c2c29d88572604
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-02-20 19:09:59 +0600

    add banner section img

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -13,7 +13,32 @@ const Banner = () => {
                     alt="" />
             </div>
 
-            <div className="absolute top-0 text-white">
+            <div className="absolute top-0 text-white pt-56 mx-auto w-full h-full text-center bg-black bg-opacity-45">
+
+                <h2 className="text-2xl text-center">
+                    Make the World Better
+                </h2>
+
+                <h2 className="text-6xl font-semibold mt-5">
+                    Reduce Your Footprint, Amplify
+                    <br />
+                    Your Impact
+                </h2>
+
+                <p className="mt-8 w-full">
+                    Luctus morbi netus lacinia sagittis fringilla viverra litora mi pellentesque adipiscing. Vitae
+                    <br />
+                    diam volutpat ultricies facilisis quisque dignissim.
+                </p>
+
+                <div>
+                    <button className="bg-[#FFC107]">
+                        Get Involved
+                    </button>
+                    <button>
+                        Learn More
+                    </button>
+                </div>
 
             </div>
         </div>
